46bde3a2554cdc5bdf8da6fedcad1bebd61aaaa7
Lucca Galhato <67028657+luccagalhato@users.noreply.github.com>
2022-09-06 15:42:49 -0300

    ..

--- server/serverModel.go
+++ server/serverModel.go
@@ -1,21 +1,11 @@
 package server
 
 import (
-	"apiCadastro/config"
-	"database/sql"
 	"net/http"
-	"net/url"
 )
 
 // Handler ...
 type Handler struct {
 	Method string
 	Fn     http.HandlerFunc
-}
-
-// SQLStr ...
-type SQLStr struct {
-	conf *config.SQL
-	url  *url.URL
-	db   *sql.DB
 }
